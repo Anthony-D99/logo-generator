ac8d06a5853f7db6b96139e9434a47644167371c
Anthony-D99 <anthonydisaf@yahoo.com>
2023-06-06 15:39:44 -0400

    logo-generator successfully created a logo properly. Now attempting to reformat code and file structure to complete acceptance criteria.

--- index.js
+++ index.js
@@ -1,42 +1,56 @@
 const inquirer = require('inquirer')
 const fs = require('fs')
-
+const shapes = require('./lib/shapes')
+let svg
 
 inquirer.prompt([
    {
       type: 'input',
       name: 'text',
-      message: '',
+      message: 'Type three letters to be included in your logo.',
    },
    {
       type: 'input',
-      name: 'text-color',
-      message: "",
+      name: 'text_color',
+      message: "Choose any color for logo's text using any color keyword or hexadecimal number.",
    },
    {
     type: 'list',
     name: 'shape',
-    message: '',
-    choices: [],
+    message: 'Select which shape you would like for your logo.',
+    choices: ['Circle',' Square', 'Triangle'],
 
    },
    {
       type: 'input',
-      name: 'shape-color',
-      message: '',
+      name: 'shape_color',
+      message: "Choose any color for your logo's background using any color keyword or hexadecimal number.",
    }
    
   
 ]).then(result => {
-   if (result.shape === '') {
-      shape = ``
-   } else if (result.shape === '') {
-      badge = ``
+   if (result.shape === 'Square') {
+      svg = shapes.circle
+   } else if (result.shape === 'Circle') {
+      svg = `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+
+      <circle cx="150" cy="100" r="80" fill="${result.shape_color}" />
+    
+      <text x="150" y="125" font-size="60" text-anchor="middle" fill="${result.text_color}">${result.text}</text>
+    
+    </svg>`
    } else {
-      badge = ``
+      svg = `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+
+      <polygon points="105,20 20,200 200,199 "
+            fill="${result.shape_color}" />
+     
+           <text x="110" y="190" font-size="60" text-anchor="middle" fill="${result.text_color}">${result.text}</text>
+     
+       </svg>`
    }
 
-   const svg = ``
+   
    fs.writeFile('logo.svg', svg, error => {
       if (error) {
          console.log(error)
